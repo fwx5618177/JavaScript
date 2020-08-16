4c1affda15aa41fee50744b900e8251eb971a6ec
fwx-gitrepo3-test5 <fwx5618177@gmail.com>
2020-08-16 18:33:52 +0900

    add func: 统计出现次数.js in 2020/08/16-18:33:48.44

---
+++ 统计本页面的字符串出现次数/统计出现次数.js
@@ -0,0 +1,16 @@
+function pageStrCount(str) {
+    var count = 0 // 统计字数
+    var allStr = document.body.innerHTML // 网页文字包含标签
+    allStr = allStr.replace(/<\w+?>/g, '').replace(/<\/\w+?>/g, '') // 过滤网页标签
+    console.log(allStr)
+    var reg = new RegExp(str, 'g') // 匹配的文字正则
+    reg.exec(allStr)
+    console.log(reg.lastIndex)
+    while (reg.lastIndex) {
+        count++
+        reg.exec(allStr)
+    }
+    console.log('字数统计结果：', count)
+}
+
+pageStrCount("x");
